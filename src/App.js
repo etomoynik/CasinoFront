98cb9b0e5c7ea6500568c2e0fdf2f2aaa5cde75b
Dima <dmitriys9701@yandex.ru>
2018-04-07 20:27:48 +0300

    from create-react-app-toolbox template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from 'react-toolbox/lib/button/Button';
+import Dialog from 'react-toolbox/lib/dialog/Dialog';
+import NavTabs from './components/NavTabs';
 import './App.css';
-import Login from './components/login'
+import AppBar from 'react-toolbox/lib/app_bar';
 
 class App extends Component {
   render() {
     return (
-      <Login/>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <Button primary raised label='Click me' />
+        </p>
+      </div>
     );
   }
 }
